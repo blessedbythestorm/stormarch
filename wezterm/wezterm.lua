9072ce761150cd6a160a9ba77a21b00036d55ae5
blessedbythestorm <omar.farahc@hotmail.com>
2024-09-03 13:34:22 +0100

    first backup

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,17 @@
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+
+config.use_fancy_tab_bar = true
+config.hide_tab_bar_if_only_one_tab = true
+config.enable_scroll_bar = true
+
+config.color_scheme = 'AdventureTime'
+config.colors = {
+  background = 'rgba(30, 30, 30, 0.5)',
+}
+
+config.window_padding = {
+  right = 8,
+}
+
+return config
