2fec6a4b07a77e302bb6e5a9a7670a5f36b9698f
blessedbythestorm <omar.farahc@hotmail.com>
2024-09-06 23:15:06 +0100

    updated configs

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+
+    alias ls='eza --icons --color'
+
+    zoxide init --cmd cd fish | source
+    starship init fish | source
 end
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,9 +5,9 @@ config.use_fancy_tab_bar = true
 config.hide_tab_bar_if_only_one_tab = true
 config.enable_scroll_bar = true
 
-config.color_scheme = 'AdventureTime'
+config.color_scheme = 'Kanagawa Dragon (Gogh)'
 config.colors = {
-  background = 'rgba(30, 30, 30, 0.5)',
+  -- background = 'rgba(30, 30, 30, 0.5)',
 }
 
 config.window_padding = {
